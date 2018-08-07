01f25dcb61d0c823cff0980f796db747e573ff95
brandonchaffee <bchaffee43@gmail.com>
2018-08-07 09:56:35 -0700

    added async to hex set pull

--- app/src/util/getContract.js
+++ app/src/util/getContract.js
@@ -24,14 +24,14 @@ let getContract = new Promise(function (resolve, reject) {
     })
   })
   .then(result => {
-    return new Promise(function (resolve, reject) {
+    return new Promise(async function (resolve, reject) {
       var hexSets = []
       for (var i = 0; i < result.setCount; i++) {
-        result.contractInstance().methods.getHexSetAt(i).call({
+        await result.contractInstance().methods.getHexSetAt(i).call({
           gas: 300000
         }, function (error, password) {
           if (error) {
-            console.log('Error in retrieving password set ' + error)
+            reject(new Error('Unable to retrieve password set'))
           } else {
             hexSets.push(password)
           }
@@ -41,39 +41,5 @@ let getContract = new Promise(function (resolve, reject) {
       resolve(result)
     })
   })
-  // .then(async function (result) {
-  //   var hexSets = []
-  //   for (let i = 0; i < result.setCount; i++) {
-  //     await new Promise(function (resolve, reject) {
-  //       result.contractInstance().methods.getHexSetAt(i).call({
-  //         gas: 300000
-  //       }, function (error, set) {
-  //         if (error) {
-  //           console.log('Error in retrieving password set ' + error)
-  //         } else {
-  //           hexSets.push(set)
-  //         }
-  //       })
-  //     })
-  //     console.log(hexSets)
-  //     return hexSets
-  //   }
-  // })
-  // return new Promise(function (resolve, reject) {
-  //   for (var i = 0; i < result.setCount; i++) {
-  //     this.$store.state.contract.instance().methods.getHexSetAt(i).call({
-  //       gas: 300000,
-  //       from: this.$store.state.web3.coinbase
-  //     }, function (error, password) {
-  //       if (error) {
-  //         console.log('Error in retrieving password set ' + error)
-  //       } else {
-  //         result = Object.assign({}, result, { setCount })
-  //         resolve(result)
-  //         this.$store.state.contract.passwordSets.push(result)
-  //       }
-  //     })
-  //   }
-  // })
 
 export default getContract
